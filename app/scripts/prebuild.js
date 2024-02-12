32630d3b5abda9ed3c6fdf932fef1a8ad456f5b1
Denton Zhou <dxz35@case.edu>
2024-02-12 13:20:17 -0500

    Update prebuild.js

--- app/scripts/prebuild.js
+++ app/scripts/prebuild.js
@@ -438,6 +438,7 @@ const importUserComponents = async () => {
           `\n\nexport { ${componentNames.join(', ')} }`
         )
       }
+      fs.rmSync('my_components', {recursive: true, force: true})
     })
   
   } catch (e) {
