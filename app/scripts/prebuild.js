9ee2a4cbde7e4b14481a754ff81ef8bfb573f027
Denton Zhou <dxz35@case.edu>
2024-02-16 00:42:43 -0500

    Update prebuild.js

--- app/scripts/prebuild.js
+++ app/scripts/prebuild.js
@@ -422,7 +422,7 @@ const createDummyComponents = async () => {
   })).then(() => {
     const tagRegex = /<\/?([a-z][a-z0-9]*)\b[^>]*>?/gi;
 
-    for (const match of markdownData.matchAll(tagRegex)) {
+    for (const match of allMarkdownData.matchAll(tagRegex)) {
       const tagName = match[1];
       fs.writeFileSync(
         `app/_components/UserComponents/${tagName}.tsx`,
